b51341be6c29ee36c9926b5c21545ba9c26eb2d2
Moncef Bettaieb <Moncef.Bettaieb@businessdecision.com>
2017-04-24 15:32:32 +0100

    test

--- src/main/java/com/businessdecision/ocp/OMS.java
+++ src/main/java/com/businessdecision/ocp/OMS.java
@@ -5,13 +5,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
 import org.json.*;
 
 import org.apache.spark.streaming.Durations;
@@ -27,6 +32,8 @@ import kafka.serializer.StringDecoder;
  * Created by Moncef.Bettaieb on 19/04/2017.
  */
 public final class OMS {
+    private static final Pattern dot = Pattern.compile(",");
+
     private OMS() {
     }
 
@@ -37,12 +44,13 @@ public final class OMS {
                     "  <topics> is a list of one or more kafka topics to consume from\n\n");
             System.exit(1);
         }
+
         String brokers = args[0];
         String topics = args[1];
 
         SparkConf sparkConf = new SparkConf().setAppName("OMS Maintenance Spark Streaming");
         JavaSparkContext conf = new JavaSparkContext(sparkConf);
-        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(30));
+        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(5));
 
         HashSet<String> topicsSet = new HashSet<String>(Arrays.asList(topics.split(",")));
         HashMap<String, String> kafkaParams = new HashMap<String, String>();
@@ -60,6 +68,7 @@ public final class OMS {
 
         JavaDStream<String> lines = messages.map(new Function<Tuple2<String, String>, String>() {
             public String call(Tuple2<String, String> tuple2) {
+                String out = "";
                 try {
                     InputStream stream = new ByteArrayInputStream(
                             tuple2._2().getBytes()
@@ -68,42 +77,13 @@ public final class OMS {
                     XZCompressorInputStream xzIn = new XZCompressorInputStream(in);
                     final byte[] buffer = new byte[Integer.MAX_VALUE];
                     int n = 0;
-                    String out = "";
+
                     while (-1 != (n = xzIn.read(buffer))) {
                         out += buffer.toString();
                         //out = out.write(buffer, 0, n);
+
                     }
-                    System.out.println(out);
-                    JSONObject obj = new JSONObject(out);
-                    String status = obj.getString("status");
-                    String date = obj.getJSONObject("end").getString("$date");
-                    String gpk = "";
-                    String rms = "";
-                    JSONArray arr = obj.getJSONArray("globals");
-                    gpk = arr.getString(0);
-                    rms = arr.getString(1);
-                    String pom = obj.getJSONObject("pom").getString("$oid");
-                    String extTemp = obj.getJSONObject("values").getString("exttemp");
-                    String taskId = obj.getJSONObject("taskid").getString("$oid");
-                    String factor = obj.getString("factor");
-                    String result = status+","+date+","+gpk+","+rms+","+pom+","+extTemp+","+taskId+","+factor;
-                    Properties props = new Properties();
-                    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.21.62.48:9092");
-                    props.put(ProducerConfig.RETRIES_CONFIG, "3");
-                    props.put(ProducerConfig.ACKS_CONFIG, "all");
-                    props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none");
-                    props.put(ProducerConfig.BATCH_SIZE_CONFIG, 200);
-                    props.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, true);
-                    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-                    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-                    KafkaProducer producer = new KafkaProducer<String,String>(props);
-                    producer.send(new ProducerRecord<String, String>("events",
-                            result));
-                    return result;
-                }
-                catch (JSONException e){
-                    System.out.println(e);
-                    return "" ;
+                    return out;
                 } catch (IOException e) {
                     System.out.println(e);
                     e.printStackTrace();
@@ -112,6 +92,24 @@ public final class OMS {
             }
         });
         lines.print();
+
+//        JavaDStream<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+//             public Iterable<String> call(String x) {
+//                                return Lists.newArrayList(dot.split(x));
+//                            }
+//         });
+//                JavaPairDStream<String, Integer> wordCounts = words.mapToPair(
+//                                new PairFunction<String, String, Integer>() {
+//                     public Tuple2<String, Integer> call(String s) {
+//                                                return new Tuple2<String, Integer>(s, 1);
+//                                            }
+//                 }).reduceByKey(
+//                                new Function2<Integer, Integer, Integer>() {
+//                     public Integer call(Integer i1, Integer i2) {
+//                                               return i1 + i2;
+//                                            }
+//                 });
+//                wordCounts.print();
 
         jssc.start();
         jssc.awaitTermination();
